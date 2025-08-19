da5f4dab7688d661b93ed85d9336cc46fe107352
loo <loofxtrt@gmail.com>
2025-08-19 13:13:20 -0300

    mostly working

--- apply.py
+++ apply.py
@@ -3,20 +3,54 @@ from pathlib import Path
 
 def get_dot_obsidian(vault_dir: Path):
     # obter o diretório .obsidian de um vault
+    # retornar diretóro caso o path passado já seja um .obsidian
     if vault_dir.name == ".obsidian":
-       return vault_dir
+        return vault_dir
+    
+    # construir o path e fazer as verificações
+    if not vault_dir.exists():
+        print(f"{vault_dir.name}: o path do vault não existe"); return False
 
-    return Path(vault_dir, ".obsidian")
+    dot_obsidian = vault_dir / ".obsidian"
+    
+    if not dot_obsidian.exists:
+        print(f"{vault_dir.name}: .obsidian não existe, vault inválido"); return False
+    
+    return dot_obsidian
+
+def get_sub_dot(vault_dir: Path, sub_dir_name: str):
+    """
+    @param vault_dir  
+        o diretório no qual a função vai obter o .obsidian e a partir daí obter o subdiretório
+        por que é usado vault_dir ao invés de um .obsidian logo?  
+        porque caso o vault não exista, essa função faz o mkdir, então isso quebra a segurança do get_dot_obsidian  
+        porque mesmo seja um vault inválido, a get_sub_dot vai fazer ele existir
+    """
+
+    # apenas prosseguir caso seja um vault válido
+    dot_obsidian = get_dot_obsidian(vault_dir)
+    if not dot_obsidian:
+        return
+
+    # obter um diretório que é filho do .obsidian, como snippets, themes, plugins etc
+    # também garante que o diretório existe antes de retornar
+    sub_dot = dot_obsidian / sub_dir_name
+    sub_dot.mkdir(parents=True, exist_ok=True)
+
+    return sub_dot
 
 def apply_snippet(target_vault: Path, css_file: Path):
     # copiar um arquivo css pro diretório de snippets de um vault
-    dir_dot = get_dot_obsidian(target_vault)
-    dir_snippets = dir_dot / "snippets"
+    dir_snippets = get_sub_dot(target_vault, "snippets")
+    if not dir_snippets:
+        return
 
     shutil.copy2(
         src=css_file,
         dst=dir_snippets
     )
+
+    print(f"{target_vault.name}: snippet {css_file.name} aplicado")
 
 def apply_theme(target_vault: Path, theme_dir: Path):
     # copiar o diretório de um tema pra um vault
@@ -27,8 +61,9 @@ def apply_theme(target_vault: Path, theme_dir: Path):
 
     # montar o caminho do diretório de forma que ele seja criado
     # e não substituído pelo copytree
-    dir_dot = get_dot_obsidian(target_vault)
-    dir_themes = dir_dot / "themes"
+    dir_themes = get_sub_dot(target_vault, "themes")
+    if not dir_themes:
+        return
 
     # sem isso, o copytree tentaria substituir a pasta .obsidian/themes
     # com a pasta do tema, ao invés de só adicionar ela
@@ -40,3 +75,5 @@ def apply_theme(target_vault: Path, theme_dir: Path):
         dst=destination,
         dirs_exist_ok=True # sobreescrever caso o tema já exista
     )
+
+    print(f"{target_vault.name}: tema {theme_dir.name} aplicado")
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from pathlib import Path
 import yaml
+import json
+from pathlib import Path
 
 import apply
 
 ALL_SNIPPETS = Path("/mnt/seagate/obsidian-central/snippets/")
 ALL_THEMES = Path("/mnt/seagate/obsidian-central/themes/")
 ALL_VAULTS = Path("/mnt/seagate/obsidian-vaults/")
+TEST_VAULTS = Path("/mnt/seagate/obsidiadididn-tests/")
 
-def run():
+def run(obsidian_root: Path, snippets_source, themes_source):
     # ler o arquivo de configuração
     with open("config.yaml", "r") as f:
         data = yaml.safe_load(f)
@@ -16,15 +18,51 @@ def run():
     # aplicando os temas, plugins e snippets especificados
     for vault_name, vault_config in data.items():
         # montar o caminho do vault atual
-        path_current_vault = ALL_VAULTS / vault_name
+        path_current_vault = obsidian_root / vault_name
+        if not path_current_vault.exists():
+            continue
+
+        # gerar os dicionários que depois serão convertidos em configs json pro vault
+        data_app = {
+            "readableLineLength": True, # concentrar o conteúdo no centro
+            "spellcheck": False,
+            "showInlineTitle": True, # mostrar o título da nota
+            "promptDelete": True, # perguntar antes de deletar pastas e arquivos
+            "attachmentFolderPath": "_attachments"
+        }
+        data_appearance = {
+            "enabledCssSnippets": [],
+            "cssTheme": None,
+            "accentColor": "",
+            "showViewHeader": True,
+            "showRibbon": True
+        }
         
         # snippets
-        for snippet in vault_config["snippets"] or []:
-            apply.apply_snippet(path_current_vault, ALL_SNIPPETS / snippet)
+        for snippet_name in vault_config["snippets"] or []:
+            # aplicar os snippets e gerar a config
+            # IMPORTANTE: no appearance.json, o snippet não deve ter .css no final
+            data_appearance["enabledCssSnippets"].append(
+                snippet_name.replace(".css", "")
+            )
+            apply.apply_snippet(path_current_vault, snippets_source / snippet_name)
 
-        # temas
-        for theme in vault_config["themes"] or []:
-            apply.apply_theme(path_current_vault, ALL_THEMES / theme)
+        # temas (com enumerate pra saber qual aplicar)
+        for index, theme_name in enumerate(vault_config["themes"] or []):
+            # aplicar todos os temas e gerar a config caso ele seja o primeiro da lista
+            if index == 0: data_appearance["cssTheme"] = theme_name
+            apply.apply_theme(path_current_vault, themes_source / theme_name)
+        
+        # escrever os arquivos de configuração do obsidian pro vault
+        this_vault_dot = apply.get_dot_obsidian(path_current_vault)
+
+        with open(this_vault_dot / "app.json", "w") as app_file:
+            json.dump(data_app, app_file, indent=4)
+            print(f"{vault_name}: app.json escrito")
+        
+        with open(this_vault_dot / "appearance.json", "w") as appearance_file:
+            json.dump(data_appearance, appearance_file, indent=4)
+            print(f"{vault_name}: appearance.json escrito")
 
 def generate_yaml_config(obsidian_root: Path):
     # ESSA FUNÇÃO ATUALMENTE SOBREESCREVE O YAML, RESETANDO ELE
@@ -52,8 +90,6 @@ def generate_yaml_config(obsidian_root: Path):
     with open("config.yaml", "w") as f:
         yaml.dump(data, f)
 
-#generate_yaml_config(
-#    obsidian_root=Path("/mnt/seagate/obsidian-vaults/"),
-#)
+#generate_yaml_config(obsidian_root=ALL_VAULTS)
 
-run()
+run(obsidian_root=TEST_VAULTS, snippets_source=ALL_SNIPPETS, themes_source=ALL_THEMES)
